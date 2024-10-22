6891d4e319b87e6ddd4dd036ec60b8e52473da10
Howpig2003 <howpig.coder@gmail.com>
2024-10-22 23:00:32 +0800

    ws

--- src/components/AnnouncementListBody.vue
+++ src/components/AnnouncementListBody.vue
@@ -10,7 +10,7 @@
         </select>
       </div>
       <div class="announcement-list">
-        <div @click="switchToShowPage(item)"class="announcement-item title" v-for="item in items" :key="item.id"><p>{{ item.title }}</p></div>
+        <div @click="switchToShowPage(item)" class="announcement-item title" v-for="item in items" :key="item.id"><p>{{ item.title }}</p></div>
       </div>
       <div class="switch-page-div">
         <button class="button" @click="toFirstPage" id="applyButton"><p>第一頁</p></button>
