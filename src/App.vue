f49e73ef266d67e123299f3d7bd1751d71aab84f
Howpig2003 <howpig.coder@gmail.com>
2024-11-07 20:33:39 +0800

    ww

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,7 @@ export default {
         .then(data => {
           // 處理 Django 回傳的 JWT
           if (data.access) {
-            this.$router.push({ name: 'MainPage' });
+            //this.$router.push({ name: 'MainPage' });
             this.access_token=data.access;
             this.ChangeUserName('已登入帳號');
             localStorage.setItem('jwt', data.access);
