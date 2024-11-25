ef94544a85aa2ae92dbb6a309e1cea516037e073
Howpig2003 <howpig.coder@gmail.com>
2024-11-25 10:41:58 +0800

    o

--- src/components/ProblemManagementPage.vue
+++ src/components/ProblemManagementPage.vue
@@ -1,111 +1,119 @@
 <template>
-    <div class="problemManagement-list-main">
-      <div class="problemManagement-list-body">
-      <h1>{{Title}}</h1>
-      <div class="radio-group">
-        <label style="user-select: none;" v-for="option in sujectOptions" :key="option.value" class="radio-option">
-          <input
-          type="radio"
-          :value="option.value"
-          v-model="selectedOption"
-          @change="updateItems"
-          name="options"
-        />
-        {{ option.text }}
-        </label>
+  <div class="problemList-list-main" v-if="isLogIn">
+    <div class="problemList-list-body">
+    <h1>{{Title}}</h1>
+    <div class="radio-group">
+      <label style="user-select: none;" v-for="option in sujectOptions" :key="option.value" class="radio-option">
+        <input
+        type="radio"
+        :value="option.value"
+        v-model="selectedOption"
+        @change="fetchData"
+        name="options"
+      />
+      {{ option.text }}
+      </label>
+    </div>
+    <div class="switch-page-div" v-if="!isMobile">
+      <label for="select" style="text-wrap: nowrap;">每頁資料筆數：</label>
+      <select id="select" v-model="itemPerPage" @change="fetchData">
+        <option  v-for="option in options" :key="option.value" :value="option.value">
+          {{ option.text }}
+        </option>
+      </select>
+    </div>
+    <div class="problemList-list">
+      <div class="problemList-item">
+        <h3>題號</h3>
+        <h3>題型</h3>
+        <h3>標題</h3>
+        <h3>作者</h3>
+        <h3>答題次數</h3>
+        <h3>正確次數</h3>
       </div>
-      <div class="switch-page-div" v-if="!isMobile">
-        <label for="select" style="text-wrap: nowrap;">每頁資料筆數：</label>
-        <select id="select" v-model="itemPerPage" >
-          <option  v-for="option in options" :key="option.value" :value="option.value">
-            {{ option.text }}
-          </option>
-        </select>
-      </div>
-      <div class="problemManagement-list">
-        <div class="problemManagement-item">
-          <h3>科目</h3>
-          <h3>題號</h3>
-          <h3>標題</h3>
-          <h3>作者</h3>
-          <h3>狀態</h3>
-          <h3>上傳時間</h3>
-          <h3>最後編輯</h3>
-        </div>
-        <div class="problemManagement-item" v-for="item in itemsOnPage" :key="item.id">
-          <p>{{ item.chineseType }}</p>
-          <p>{{ item.id }}</p>
-          <p class="title" @click="switchToEditPage(item)">{{ item.title }}</p>
-          <p>{{ item.author }}</p>
-          <p>{{ item.state }}</p>
-          <p>{{ item.createDate }}</p>
-          <p>{{ item.lastEditDate }}</p>
-        </div>
-      </div>
-      <div class="switch-page-div">
-        <button class="button" @click="toFirstPage" id="applyButton"><p>第一頁</p></button>
-        <button class="button" @click="previousPage" id="applyButton"><p>上一頁</p></button>
-        <p style="color:rgb(93, 153, 175);">第</p>
-        <input @change="applyInput" class="input-box" type="number" name="" id="" v-model="inputPage">
-        <p style="color:rgb(93, 153, 175);">頁</p>
-         <!-- <button class="button" style="margin :0px 6px;" @click="applyInput" id="applyButton"><p>前往</p></button> -->
-        <button class="button" @click="nextPage" id="applyButton"><p>下一頁</p></button>
-        <button class="button" @click="toLastPage" id="applyButton"><p>最後一頁</p></button>
+      <div class="problemList-item" v-for="item in items" :key="item.id">
+        <p v-if="selectedOption!='program'">{{ item.problem_id }}</p>
+        <p v-if="selectedOption!='program'">{{ item.problem_type }}</p>
+        <p v-if="selectedOption!='program'" class="title" @click="switchToShowPage(item)">{{ item.problem_id }}</p>
+        <p v-if="selectedOption!='program'">{{ item.contributor_id }}</p>
+        <p v-if="selectedOption!='program'">{{ item.submission_count }}</p>
+        <p v-if="selectedOption!='program'">{{ item.correct_count }}</p>
+
+        <p v-if="selectedOption=='program'">{{ item.problem_id }}</p>
+        <p v-if="selectedOption=='program'">{{ item.problem_type }}</p>
+        <p v-if="selectedOption=='program'" class="title" @click="switchToShowPage(item)">{{ item.title }}</p>
+        <p v-if="selectedOption=='program'">{{ item.author }}</p>
+        <p v-if="selectedOption=='program'">{{ item.submit_count }}</p>
+        <p v-if="selectedOption=='program'">{{ item.AC_count }}</p>
       </div>
     </div>
-    <div class="problemManagement-list-body-phone" v-if="isMobile">
-      <h1>{{Title}}</h1>
-      <div class="radio-group">
-        <label style="user-select: none;" v-for="option in sujectOptions" :key="option.value" class="radio-option">
-          <input
-          type="radio"
-          :value="option.value"
-          v-model="selectedOption"
-          @change="updateItems"
-          name="options"
-        />
-        {{ option.text }}
-        </label>
-      </div>
-      <div class="switch-page-div">
-        <label for="select" style="text-wrap: nowrap;">每頁資料筆數：</label>
-        <select id="select" v-model="itemPerPage" >
-          <option  v-for="option in options" :key="option.value" :value="option.value">
-            {{ option.text }}
-          </option>
-        </select>
-      </div>
-      <div class="problemManagement-list">
-        <div class="problemManagement-item">
-          <h3>科目</h3>
-          <h3>題號</h3>
-          <h3>標題</h3>
-          <h3>狀態</h3>
-        </div>
-        <div class="problemManagement-item" v-for="item in itemsOnPage" :key="item.id">
-          <p>{{ item.chineseType }}</p>
-          <p>{{ item.id }}</p>
-          <p class="title" @click="switchToEditPage(item)">{{ item.title }}</p>
-          <p>{{ item.state }}</p>
-        </div>
-      </div>
-      <div class="switch-page-div">
-        <button class="button" @click="toFirstPage" id="applyButton"><p>第一頁</p></button>
-        <button class="button" @click="previousPage" id="applyButton"><p>上一頁</p></button>
-        <p style="color:rgb(93, 153, 175);">第</p>
-        <input @change="applyInput" class="input-box" type="number" name="" id="" v-model="inputPage">
-        <p style="color:rgb(93, 153, 175);">頁</p>
-         <!-- <button class="button" style="margin :0px 6px;" @click="applyInput" id="applyButton"><p>前往</p></button> -->
-        <button class="button" @click="nextPage" id="applyButton"><p>下一頁</p></button>
-        <button class="button" @click="toLastPage" id="applyButton"><p>最後一頁</p></button>
-      </div>
-    </div>  
+    <div class="switch-page-div">
+      <button class="button" @click="toFirstPage" id="applyButton"><p>第一頁</p></button>
+      <button class="button" @click="previousPage" id="applyButton"><p>上一頁</p></button>
+      <p style="color:rgb(93, 153, 175);">第</p>
+      <input @change="applyInput" class="input-box" type="number" name="" id="" v-model="inputPage">
+      <p style="color:rgb(93, 153, 175);">頁</p>
+       <!-- <button class="button" style="margin :0px 6px;" @click="applyInput" id="applyButton"><p>前往</p></button> -->
+      <button class="button" @click="nextPage" id="applyButton"><p>下一頁</p></button>
+      <button class="button" @click="toLastPage" id="applyButton"><p>最後一頁</p></button>
     </div>
-  </template>
+  </div>
+  <div class="problemList-list-body-phone" v-if="isMobile">
+    <h1>{{Title}}</h1>
+    <div class="radio-group">
+      <label style="user-select: none;" v-for="option in sujectOptions" :key="option.value" class="radio-option">
+        <input
+        type="radio"
+        :value="option.value"
+        v-model="selectedOption"
+        @change="fetchData"
+        name="options"
+      />
+      {{ option.text }}
+      </label>
+    </div>
+    <div class="switch-page-div">
+      <label for="select" style="text-wrap: nowrap;">每頁資料筆數：</label>
+      <select id="select" v-model="itemPerPage" @change="fetchData">
+        <option  v-for="option in options" :key="option.value" :value="option.value">
+          {{ option.text }}
+        </option>
+      </select>
+    </div>
+    <div class="problemList-list">
+      <div class="problemList-item">
+        <h3>題號</h3>
+        <h3>題型</h3>
+        <h3>標題</h3>
+        <h3>作者</h3>
+      </div>
+      <div class="problemList-item" v-for="item in items" :key="item.id">
+        <p>{{ item.problem_id }}</p>
+        <p>{{ item.problem_type }}</p>
+        <p class="title" @click="switchToShowPage(item)">{{ item.title }}</p>
+        <p>{{ item.author }}</p>
+      </div>
+    </div>
+    <div class="switch-page-div">
+      <button class="button" @click="toFirstPage" id="applyButton"><p>第一頁</p></button>
+      <button class="button" @click="previousPage" id="applyButton"><p>上一頁</p></button>
+      <p style="color:rgb(93, 153, 175);">第</p>
+      <input @change="applyInput" class="input-box" type="number" name="" id="" v-model="inputPage">
+      <p style="color:rgb(93, 153, 175);">頁</p>
+       <!-- <button class="button" style="margin :0px 6px;" @click="applyInput" id="applyButton"><p>前往</p></button> -->
+      <button class="button" @click="nextPage" id="applyButton"><p>下一頁</p></button>
+      <button class="button" @click="toLastPage" id="applyButton"><p>最後一頁</p></button>
+    </div>
+  </div>  
+  </div>
+  <div v-if="!isLogIn" class="no-data">
+    <h3>請登入查看資料</h3>
+  </div>
+</template>
   
   <script>
   export default {
-    name: 'AnnouncementListBody',
+    name: 'ProblemManagementListBody',
     data(){
       return {
         items:[],
@@ -121,12 +129,15 @@
           { value: 100, text: '100' },
         ],
         sujectOptions: [
-          { value: 'all', text: '全部顯示' },
           { value: 'program', text: '程式' },
           { value: 'math', text: '數學' },
-          { value: 'natural', text: '自然' },
+          { value: 'science', text: '自然' },
         ],
-        selectedOption: 'all',
+        requestBody: {
+          'request_page': 1,
+          'request_count': 10 
+        },
+        selectedOption: 'program',
         isMobile:false,
       }
     },
@@ -139,7 +150,7 @@
     computed:{
       itemsOnPage(){
         const res_item = [];
-        for(let i=this.page*this.itemPerPage-this.itemPerPage;i<this.page*this.itemPerPage;i++){
+        for(let i=0;this.itemPerPage;i++){
           if(this.itemsWithType[i]==undefined) break;
           if(this.itemsWithType[i].type==this.selectedOption || this.selectedOption=='all'){
             res_item.push(this.itemsWithType[i]);
@@ -150,70 +161,53 @@
       },
     },
     methods:{
-      updateItems(){
-        const res_item = [];
-        for(let i=0;i<this.items.length;i++){
-          if(this.items[i]==undefined) break;
-          if(this.items[i].type==this.selectedOption || this.selectedOption=='all'){
-            res_item.push(this.items[i]);
+      async fetchData() {
+        try {
+          const queryParams = new URLSearchParams({
+            request_page: this.page,
+            request_count: this.itemPerPage,
+          }).toString();
+          const request_type = this.selectedOption;
+          const token=this.access_token;
+          const response = await fetch(`http://127.0.0.1:60000/teacher-platform/${request_type}-problem-info-list/?${queryParams}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            }, 
+          });
+
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
           }
+
+          const result = await response.json();
+          this.items = result.problem_list; // 將獲取的問題列表存儲到 itemsWithType
+        } catch (error) {
+          console.error('發送請求時出錯：', error);
         }
-        this.itemsWithType=res_item;
-        this.page=1;
+      },
+      updateItems(){
+        this.fetchData();
       },
       getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
       },
-      generateItems() {
-        const items = [];
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 月份從0開始，所以需要加1
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const dateString = `${year}-${month}-${day}`;
-
-        for (let i = 1; i <= 10000; i++) {
-          const idx=this.getRandomInt(0,3);
-          const sujectArray=["program","math","natural"];
-          const ChinesesujectArray=["程式","數學","自然"];
-          items.push({
-            id: i,
-            title: `Title ${i}`,
-            author: `Author ${i}`,
-            lastEditDate: dateString,
-            createDate: dateString,
-            state:'編輯中',
-            type: sujectArray[idx],
-            chineseType: ChinesesujectArray[idx],
-          });
-        }
-        return items;
-      },
       applyInput(){
         this.page=this.inputPage;
-        if(this.page<=0)
-        {
-          this.page=1;
-          this.inputPage=this.page;
-        }
-        else if(this.page>Math.ceil(this.itemsWithType.length/this.itemPerPage))
-        {
-          this.page=Math.ceil(this.itemsWithType.length/this.itemPerPage);
-          this.inputPage=this.page;
-        }
+        this.updateItems();
       },
       nextPage(){
-        if(this.page<Math.ceil(this.itemsWithType.length/this.itemPerPage))
-        {
-          this.page+=1;
-          this.inputPage=this.page;
+        if(this.items.length>0){
+          this.inputPage+=1;
+          this.applyInput();
         }
       },
       previousPage(){
         if(this.page>1)
         {
-          this.page-=1;
-          this.inputPage=this.page;
+          this.inputPage-=1;
+          this.applyInput();
         }
       },
       toFirstPage(){
@@ -221,18 +215,18 @@
         this.applyInput();
       },
       toLastPage(){
-        this.inputPage=Math.ceil(this.itemsWithType.length/this.itemPerPage);
+        //暫時沒有功能，之後跟後端請求頁數
         this.applyInput();
       },
-      switchToEditPage(item){
-        if(item.type=='program'){
-          this.$router.push({ name: 'ProgramProblemEdit', params: { id:item.id} ,query:{title:item.title,suject:item.type}});
+      switchToShowPage(item){
+        if(this.selectedOption=='program'){
+          this.$router.push({ name: 'ProgramProblemShow', params: { id:item.problem_id} ,query:{suject:this.selectedOption}});
         }
-        else if(item.type=='math'){
-          this.$router.push({ name: 'MathProblemEdit', params: { id: item.id} ,query:{title:item.title,suject:item.type}});
+        else if(this.selectedOption=='math'){
+          this.$router.push({ name: 'MathProblemShow', params: { id: item.problem_id} ,query:{suject:this.selectedOption}});
         }
-        else if(item.type=='natural'){
-          this.$router.push({ name: 'NaturalProblemEdit', params: { id: item.id},query:{title:item.title,suject:item.type} });
+        else if(this.selectedOption=='science'){
+          this.$router.push({ name: 'NaturalProblemShow', params: { id: item.problem_id},query:{suject:this.selectedOption}});
         }
       },
       checkScreenSize() {
@@ -240,10 +234,11 @@
         this.isMobile = window.innerWidth <= 1050;
       }
     },
+    inject:['access_token','isLogIn'],
     mounted() {
+      
       if (this.items.length === 0) {
-        this.items = this.generateItems();
-        this.updateItems();
+        this.fetchData();
       }
       this.checkScreenSize();
       window.addEventListener('resize', this.checkScreenSize);
@@ -257,7 +252,14 @@
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
-  .problemManagement-list-main{
+  .no-data{
+    height: 80%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .problemList-list-main{
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -313,13 +315,13 @@
     display: flex;
     align-items: center;
   }
-  .problemManagement-list-body {
+  .problemList-list-body {
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 100%;
   }
-  .problemManagement-list-body-phone {
+  .problemList-list-body-phone {
     display: none;
     flex-direction: column;
     align-items: center;
@@ -338,15 +340,15 @@
   a {
     color: #42b983;
   }
-  .problemManagement-list {
+  .problemList-list {
     display: flex;
     flex-direction: column;
     width: 85%;
     padding: 10px;
   }
-  .problemManagement-item {
+  .problemList-item {
     display: grid;
-    grid-template-columns: 2fr 2fr 6fr 4fr 3fr 4fr 4fr;
+    grid-template-columns: 2fr 2fr 6fr 4fr 2fr 2fr;
     min-height: 40px;
     border-bottom: 1px solid rgb(175, 175, 175);
     align-items: center;
@@ -364,16 +366,16 @@
     font-weight: bold;
   }
   @media (max-width: 1050px){
-    .problemManagement-list-main{
+    .problemList-list-main{
       width: 100% !important;
     }
-    .problemManagement-list-body-phone{
+    .problemList-list-body-phone{
       display: flex;
     }
-    .problemManagement-list-body{
+    .problemList-list-body{
       display: none;
     }
-    .problemManagement-item {
+    .problemList-item {
     display: grid;
     grid-template-columns: 2fr 2fr 6fr 3fr;
     min-height: 40px;
