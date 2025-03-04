2c9b796b7d630cc51b3d43da10a3935b14e8cce2
Howpig2003 <106654207+Howpig2003@users.noreply.github.com>
2025-03-04 19:13:49 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from '../components/MainBody.vue'
 import MainPage2 from '../components/MainBody2.vue'
 import MainPage3 from '../components/MainBody3.vue'
@@ -136,7 +136,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
